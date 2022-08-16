d0b4540b4c05f9dd2460156d2266255768aea029
dagp-ucol <dgentner@ucol.mx>
2022-08-15 19:07:42 -0500

    - Allow to print grid and start the game
    Co-authored-by: felipelopezoa <lopezortegaaceves@gmail.com>
    Co-authored-by: jorgeboris <eduardo.borisq@gmail.com>
    Co-authored-by: EduardoBriceno89 <eduardbrice@outlook.com>
    Co-authored-by: CharlyLuna <cluna10@ucol.mx>

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,45 +1,56 @@
 class Cell
 
-    attr_accessor :alive, :neighbors
+	attr_accessor :alive, :neighbors
 
-    def initialize
-    
-        @alive = false
-        @next_state = false
-        @neighbors = 0
+	def initialize
 
-    end
+		@alive = false
+		@next_state = false
+		@neighbors = 0
 
-    def change_life (value)
-    
-        @next_state = value
+	end
 
-    end
+	def change_life (value)
 
-    def  to_string
+		@next_state = value
 
-        @alive ? "*" : "."
+	end
 
-    end
+	def  to_string
+
+		@alive ? "*" : "."
+
+	end
 
 
 end
 
 class Grid
 
-    def initialize(width, height)
+	attr_accessor :width, :height, :grid
 
-        @width = width
-        @height = height
-        @grid = create_grid
-        
-    end
+	def initialize(width, height)
 
-    def create_grid
+		@width = width
+		@height = height
+		@grid = create_grid
 
-        @array = Array.new(@height, Array.new(@width){Cell.new})
+	end
 
-    end
+	def create_grid
+
+		@array = Array.new(@height, Array.new(@width){Cell.new})
+		@array
+
+	end
+
+	def update_grid
+		@grid.each do |row|
+			row.each do |cell|
+				grid[row][cell].alive = grid[row][cell].next_state
+			end
+		end
+	end
 
 
 
@@ -48,54 +59,88 @@ end
 
 class Game
 
-    def initialize(input)
+	def initialize(input)
 
-        @grid= Grid.new(input[0].length, input.length)
-    
-    end
+		puts input[0].length
+		puts input.length
+		@grid= Grid.new(input[0].length, input.length)
+		@grid = @grid.grid
+		puts @grid
 
-end 
+	end
+
+	def start
+		Printer.new(@grid)
+		Iteration.new(@grid)
+		@grid.update_grid
+	end
+
+end
 
 class Iteration
 
-    def initialize (grid)
+	def initialize (grid)
 
-        @grid = grid
+		@grid = grid
+		check_status
 
-    end
+	end
 
-    def check_status
+	def check_status
 
-        @grid.height.times do |row|
-            @grid.width.times do |column|
+			@grid.height.times do |row|
+				@grid.width.times do |column|
 
-                cell = @grid[row][column]
+					cell = @grid[row][column]
 
+					check_neighbors(column, row)
+					cell.nextState = rules(cell)
 
-            end 
-        end
-
-
-    end 
-
-    def check_neighbors(col,row)
-        directions = [[-1,-1],[0,-1],[+1,-1],[-1,0],[+1,0],[-1,+1],[0,+1], [+1,+1] ]
-            directions.each do |x,y|
-                check_direction(col+x,row+y) && @grid[x][y].alive ? @grid[col][row].neighbors += 1 :  @grid[col][row].neighbors += 0
-            end
-        end 
-    end
-
-    def check_direction(x,y)
-        return (x>=0 && y>=0 && x<@grid.width && y<@grid.height)
-    end
+				end
+			end
 
 
-end 
+	end
+
+	def check_neighbors(col,row)
+		directions = [[-1,-1],[0,-1],[+1,-1],[-1,0],[+1,0],[-1,+1],[0,+1], [+1,+1] ]
+		directions.each do |x,y|
+			check_direction(col+x,row+y) && @grid[x][y].alive ? @grid[col][row].neighbors += 1 :  @grid[col][row].neighbors += 0
+		end
+	end
+
+	def check_direction(x,y)
+		return (x>=0 && y>=0 && x<@grid.width && y<@grid.height)
+	end
+
+	def rules(cell)
+		neighbor = cell.neighbor
+		(cell.alive? && [2, 3].include?(neighbor) || (!cell.alive? && neighbor == 3))
+	end
+
+
+end
+
+class Printer
+
+	def initialize(grid)
+		grid.height.times do |i|
+			grid.width.times do |j|
+				puts grid[i][j].to_string
+			end
+			puts ""
+	end
+	puts " "
+	end
+
+end
 
 def open_file(ruta)
-    file = File.open(ruta)
-    file.readlines.map(&:chomp)
-    end
-    
-    data = open_file('generation.text')
+	file = File.open(ruta)
+	file.readlines.map(&:chomp)
+end
+
+	data = open_file('./grid.txt')
+
+game = Game.new(data)
+game.start
